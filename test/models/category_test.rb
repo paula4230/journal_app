291b6fe6b8d887fb2953acdccf695d415a3251c0
pauline_grace <pedepolonia@up.edu.ph>
2021-07-13 00:55:14 +0800

    add testing for model category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,5 +4,9 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  
+  test 'should not save Category without title' do
+    category = Category.new 
+
+    assert_not category.save
+  end
 end
