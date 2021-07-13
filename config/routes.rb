4394135670cf7ab8dc8ccc96b59be0807b295a3c
pauline_grace <pedepolonia@up.edu.ph>
2021-07-13 18:45:17 +0800

    add route for users to sign

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+
   
   resources :categories do
     resources :tasks
   end
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   
+  devise_scope :user do
+    get "/users/sign_in" => "users/sessions#new"
+  end
 
 
 end
