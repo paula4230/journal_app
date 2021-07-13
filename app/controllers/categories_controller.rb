7b3611c4e8938b162e53783a254a32a5e8fe29a3
pauline_grace <pedepolonia@up.edu.ph>
2021-07-13 19:47:22 +0800

    add controller for updating category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,41 +1,40 @@
 class CategoriesController < ApplicationController
-    before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :set_category, only: %i[ show edit update destroy ]
 
-    def index
-      @categories = Category.all
-    end
+  def index
+    @categories = Category.all
+  end
   
-    def show
-    end
+  def show
+  end
   
-    def new
-      @category = Category.new
-    end
+  def new
+    @category = Category.new
+  end
   
-    def edit
-    end
+  def edit
+  end
   
-    def create
-      @category = Category.new(category_params)
+  def create
+    @category = Category.new(category_params)
   
-      respond_to do |format|
-        if @category.save
-          redirect_to @category, notice: "Category was successfully created." 
-        else
-            render :new, status: :unprocessable_entity 
-        end
-      end
+
+    if @category.save
+      redirect_to @category, notice: "Category was successfully created." 
+    else
+      render :new, status: :unprocessable_entity 
     end
+  end
+
   
-    def update
-      respond_to do |format|
-        if @category.update(category_params)
-            redirect_to @category, notice: "Category was successfully updated." 
-        else
-          render :edit, status: :unprocessable_entity 
-        end
-      end
+  def update
+    if @category.update(category_params)
+        redirect_to @category, notice: "Category was successfully updated." 
+    else
+      render :edit, status: :unprocessable_entity 
     end
+  end
+
   
     def destroy
       @category.destroy
