516f434c7ef4578e268099a9009d15937460118c
pauline_grace <pedepolonia@up.edu.ph>
2021-07-13 16:54:59 +0800

    add testing for model Task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,11 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should not save Task without title' do
+    task = Task.new 
+
+    assert_not task.save
+  end
+
 end
