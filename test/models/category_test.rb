d0df7e33453267285ed1868ee9f31dd1318e40b8
pauline_grace <pedepolonia@up.edu.ph>
2021-07-18 01:35:23 +0800

    add validation for duplicate title under model category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,16 +1,23 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  test 'should not save Category without title' do
-    category = Category.new 
+  setup do
+    users(:lisa).password = 'string'
+  end
 
+  test 'should not save Category without title' do
+    category = Category.new(user: users(:lisa))
     assert_not category.save
   end
 
+  test 'should not save category if the title has already been taken' do
+    category1 = Category.new(title:"work", user: users(:lisa))
+    category2 = Category.new(title:"work", user: users(:lisa))
 
+    if category1 == category2
+      assert_not category2.save 
+    end     
+  end
 end
 
 
