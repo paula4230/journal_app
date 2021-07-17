cb0d043a09c6d08cf4851aa4f282f62b3e793ac5
pauline_grace <pedepolonia@up.edu.ph>
2021-07-18 02:01:32 +0800

    add validation for due

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,14 @@ class Task < ApplicationRecord
     belongs_to :category
     belongs_to :user
 
-    validates :title, presence: true, uniqueness: true
+    validates :title, presence: true
+    validates_uniqueness_of :title, scope: :category_id
+
+    include ActiveModel::Validations
+    validates_with DateValidator
+
+    
+
 end
+
+
