578ce921266e32ebf0ffea775adf6c3b2d6c987b
pauline_grace <pedepolonia@up.edu.ph>
2021-07-18 02:00:46 +0800

    add validation for category to prevent saving blacnk or duplcate titles

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     belongs_to :user
     has_many :tasks
-    validates :title, presence: true, uniqueness: true
+    validates :title, presence: true
+    validates_uniqueness_of :title, scope: :user_id
 end
