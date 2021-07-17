261c940322ddeea1c68e60423551ba0465a397ac
pauline_grace <pedepolonia@up.edu.ph>
2021-07-18 01:37:10 +0800

    add testing for login form

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -6,6 +6,22 @@ class UserTest < ActionDispatch::IntegrationTest
     assert_select "h2", text: "Log in"
   end
 
+  test "login form should be present so the user can sign in" do
+    get "/users/sign_in"  
+    assert_select "form"
+  end
+
+  test "email should not be empty in the login form" do
+    get "/users/sign_in"
+    assert_response :success 
+
+    post "/users/sign_in",
+      params: { users: {email: nil}}
+      assert_response :success 
+    end 
+
+
+
   
 
 
