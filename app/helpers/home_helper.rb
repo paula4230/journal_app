6db97a091a611d4bcffe204cdbdab73b00d628a9
pauline_grace <pedepolonia@up.edu.ph>
2021-07-18 01:58:59 +0800

    comment out controllers in home helper file

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,16 @@
 module HomeHelper
+
+    # def get_all_categories
+    #       @categories = Category.all
+    # end
+
+    # def get_all_tasks
+    #     @tasks = Task.all
+    # end
+
+    # def get_urgent_tasks
+    #     @urgent_tasks = Task.where(due: Date.today)
+    # end
+
+
 end
