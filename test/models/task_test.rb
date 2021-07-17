74e0ec371126de0b8441bf451ec9703bf6b4b069
pauline_grace <pedepolonia@up.edu.ph>
2021-07-18 01:36:00 +0800

    add validation for duplicate title under model task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,12 +1,28 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    users(:lisa).password = 'string'
+  end
 
   test 'should not save Task without title' do
-    task = Task.new 
+    task = Task.new(category: categories(:work), user: users(:lisa))
+    assert_not task.save, "saved task without title"
+  end
+
+  test 'should not save task if the title under the same category has already been taken' do
+
+    task1 = Task.new(title: "testing", category: categories(:work), user: users(:lisa))
+    task1.save
+    task2 = Task.new(title: "testing", category: categories(:work), user: users(:lisa))
+
+      if task1.valid?
+        assert_not task2.save
+      end   
+  end
+
+  test "due date should not be less than the date today" do
+    task = Task.new(title: "testing", due: Date.new(2021, 7, 16), category: categories(:work), user: users(:lisa))  
 
     assert_not task.save
   end
