390f43871eb3340d896e522313b742badaa8358f
pauline_grace <pedepolonia@up.edu.ph>
2021-07-18 12:54:02 +0800

    add test for user login

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -1,28 +1,30 @@
 require "test_helper"
 
 class UserTest < ActionDispatch::IntegrationTest
-  test "visiting the users sign in page" do
-    get "/users/sign_in"  
-    assert_select "h2", text: "Log in"
-  end
-
-  test "login form should be present so the user can sign in" do
-    get "/users/sign_in"  
-    assert_select "form"
-  end
-
+  
   test "email should not be empty in the login form" do
     get "/users/sign_in"
     assert_response :success 
 
     post "/users/sign_in",
-      params: { users: {email: nil}}
-      assert_response :success 
-    end 
+    params: { users: {email: ""}}
+    assert_not_empty(:email)
+  end
+
+  test "user should be able to sign in" do
+    get "/users/sign_in"  
+    assert_select "form"
+
+
+    post "/users/sign_in",
+    params: { users: {email: 'lisa@yg.com' }}
+    get user_session_path
+    assert_response :success
+  end
 
 
 
-  
+
 
 
 end
