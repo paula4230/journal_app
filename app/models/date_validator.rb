88a3e6a8927c595b9694c60b218d2d0a20e4c546
pauline_grace <pedepolonia@up.edu.ph>
2021-07-21 18:59:28 +0800

    refactor date validation for task

--- app/models/date_validator.rb
+++ app/models/date_validator.rb
@@ -1,6 +1,7 @@
 class DateValidator < ActiveModel::Validator
     def validate(task)
-        if  task.due < Date.current 
+    
+        if task.due < Date.current
             task.errors.add :due, "Date should not be in the past"
         end 
     end
